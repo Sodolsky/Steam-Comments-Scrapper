cfe3501444ad7f0a9b91594afd1b69993380f7e6
Sodolsky <mateuszsodolskiofficial@gmail.com>
2023-04-05 20:43:25 +0200

    Formatted the code but it stopped working will fix later

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 import puppeteer from "puppeteer";
 import fs from "fs";
-(async () => {
+const getUserComments = async () => {
   const browser = await puppeteer.launch();
-  const writeStream = fs.createWriteStream("comments.txt");
   const page = await browser.newPage();
-  const defaultLink =
-    "https://steamcommunity.com/profiles/76561199003638675/allcomments";
-  await page.goto(`${defaultLink}?ctp=1`);
-  const nextPageSelector =
-    "#commentthread_Profile_76561199003638675_pagebtn_next";
+  //? Here you paste user steam profile
+  const userLink = "https://steamcommunity.com/profiles/76561199003638675";
+  const formattedLink = `${userLink}/allcomments`;
+  await page.goto(`${formattedLink}`);
+  const nextPageSelector = ".pagebtn";
+  //#commentthread_Profile_76561198163405426_pagebtn_next
   const commentSelector = ".commentthread_comment_text";
+  const pageSelectors = await page.$$(".commentthread_pagelink");
+  let numberOfPages = 0;
+  if (pageSelectors.length > 1) {
+    numberOfPages = await pageSelectors[pageSelectors.length - 1].evaluate(
+      (node) => node.textContent
+    );
+  }
+
   const arrayOfComments = [];
   await page.setViewport({ width: 1080, height: 1024 });
-  for (let i = 0; i < 188; i++) {
-    const nextPageSelectorButton = await page.$(nextPageSelector);
+  for (let i = 0; i < numberOfPages; i++) {
+    console.log(page.url());
+    console.log(`Progress:${(i / numberOfPages).toFixed(2) * 100}%`);
+    const pageSwitchButtons = await page.$$(nextPageSelector);
+    const nextPageSelectorButton =
+      pageSwitchButtons[pageSwitchButtons.length - 1];
     const selectors = await page.$$(commentSelector);
-    console.log(i);
     //Extracting text from all elements
     const localArr = [];
     for (let i = 0; i < selectors.length; i++) {
@@ -25,13 +36,29 @@ import fs from "fs";
       localArr.push(cleanedString);
     }
     arrayOfComments.push(localArr);
+    const classList = await page.evaluate(
+      (button) => Array.from(button.classList),
+      nextPageSelectorButton
+    );
+    //Check if switching pages button is disabled if is is break the loop
+    const isDisabled = classList.some((x) => x === "disabled");
+    if (isDisabled) {
+      break;
+    }
+    //If the button can still be clicked switch the page and collect more comments
     await nextPageSelectorButton.click();
     await page.waitForNavigation();
     await page.waitForSelector(nextPageSelector);
   }
+  //Write to file
+  writeToFile(arrayOfComments);
+  await browser.close();
+};
+const writeToFile = async (arrayOfComments) => {
+  const writeStream = fs.createWriteStream("comments.txt");
   arrayOfComments.flat(2).forEach((comment) => {
     writeStream.write(comment + "\n");
   });
   writeStream.end();
-  await browser.close();
-})();
+};
+getUserComments();
